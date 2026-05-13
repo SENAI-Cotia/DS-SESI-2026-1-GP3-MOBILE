a7372dc37bb483b9440269c6b7c8a9565abd7914
Gabriel Bento <greninzard12@gmail.com>
2026-05-13 10:16:58 -0300

    Update pesquisar.tsx

--- app/(tabs)/pesquisar.tsx
+++ app/(tabs)/pesquisar.tsx
@@ -1,10 +1,8 @@
-import { Text, View } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-
-export default function Index() {
-    return (
-        <View>
-            <Text>Pesquisar</Text>
-        </View>
-    );
+export default function Pesquisar() {
+    const router = useRouter();
 }
